b0b2a023a7b79cf877c52316d9fe3ca006312120
Veronica Martinez <vmartinez6615@gmail.com>
2021-01-15 11:31:41 -0500

    fixing bug

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -75,3 +75,15 @@ test("uses a potion from inventory", () => {
 
   expect(player.inventory.length).toBeLessThan(oldCount);
 });
+test("subtracts from player's health", () => {
+  const player = new Player("Dave");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const Game = require("./lib/Game");
+const Game = require("./lib/Game.js");
 
 new Game().initializeGame();
